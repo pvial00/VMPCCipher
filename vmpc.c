262c138f9125af5235d48f8232da1f85140bcb1d
Karl <pvial@krypt.local>
2018-01-16 17:03:27 -0800

    v0.0.0

--- vmpc.c
+++ vmpc.c
@@ -27,32 +27,24 @@ int main(int argc, char *argv[]) {
     fseek(infile, 0, SEEK_SET);
     data = malloc(fsize);
     fread(data, 1, fsize, infile);
-    printf("%lu\n", strlen(data));
-    printf("%li\n", fsize);
     fclose(infile);
-    outfile = fopen(out, "w");
+    outfile = fopen(out, "wb");
     if (strcmp(mode, "encrypt") == 0) {
         buf = malloc(fsize);
         randfile = fopen("/dev/urandom", "rb");
-        ssize_t result = fread(&iv, 1, iv_length, randfile);
+        fread(&iv, 1, iv_length, randfile);
         fclose(randfile);
-        printf("%s\n", iv);
-        printf("%lu\n", strlen(iv));
         ksa(K, iv);
         crypt(data, buf, fsize);
         fwrite(iv, 1, iv_length, outfile);
-        printf("%lu\n", strlen(data));
         fwrite(buf, 1, fsize, outfile);
         free(data);
         free(buf);
     }
     else if (strcmp(mode, "decrypt") == 0) {
-        /*memcpy(iv, &data[0], iv_length); */
         for (i = 0; i < iv_length; i++) {
             iv[i] = data[i];
         }
-        printf("%s\n", iv);
-        printf("%lu\n", strlen(iv));
         unsigned char *msg = NULL;
         msg = malloc((fsize - iv_length));
         for (i = iv_length; i < (fsize - iv_length); i++) {
@@ -60,10 +52,7 @@ int main(int argc, char *argv[]) {
             x++;
         }
         free(data);
-        /* memcpy(msg, &data[iv_length], (fsize  - iv_length)); */
-        /* memcpy(msg, &data[iv_length], (fsize - iv_length)); */
         ksa(K, iv);
-        printf("%lu\n", strlen(msg));
         crypt(msg, msg, (fsize - iv_length));
         fwrite(msg, 1, (fsize - iv_length), outfile);
         free(msg);
